ba1a87d81fbeeb4a01a8599179563656533ac090
André Nanninga <andre@nannin.ga>
2019-12-29 08:43:10 +0100

    chore: basic setup

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style.css";
+
+const About = () => (
+  <div className="min-h-screen bg-blue-100">
+    <main className="container mx-auto">
+      <h1 className="text-4xl font-serif">About us</h1>
+    </main>
+  </div>
+);
+
+export default About;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../style.css";
+
+const Home = () => (
+  <main className="container mx-auto">
+    <h1 className="text-4xl font-serif">Hello world!</h1>
+  </main>
+);
+
+export default Home;
