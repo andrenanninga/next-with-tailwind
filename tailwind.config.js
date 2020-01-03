3019bea4fe1c6613044cad75f34e4235b5fb3894
André Nanninga <andre@nannin.ga>
2020-01-03 12:57:59 +0100

    chore: remove fallback variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,5 @@
 module.exports = {
   theme: {
     extend: {}
-  },
-  variants: {
-    flexDirection: ["responsive"],
-    padding: ["fallback", "default", "responsive"],
-    margin: ["fallback", "default", "responsive"]
-  },
-  plugins: [
-    function({ addVariant, e }) {
-      addVariant("fallback", ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-          return `.${e(`fallback${separator}${className}`)}`;
-        });
-      });
-    }
-  ]
+  }
 };
